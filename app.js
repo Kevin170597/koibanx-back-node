08e4bcd5d68caed048f91ff8cb6a36bd8f91b6f1
Kevin <kevin@xymilar.com>
2022-06-20 20:57:04 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const mongoosePaginate = require('mongoose-paginate-v2');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const url = process.env.MONGO;
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('connected'))
+.catch((err) => console.log(err))
+
+const shopSchema = mongoose.Schema({
+    _id: String,
+    commerce: String,
+    cuit: String,
+    concept1: Number,
+    concept2: Number,
+    concept3: Number,
+    concept4: Number,
+    concept5: Number,
+    concept6: Number,
+    current_balance: Number,
+    active: Boolean,
+    last_sale: String
+}, { versionKey: false })
+
+shopSchema.plugin(mongoosePaginate);
+
+const shopMdel = mongoose.model('shops', shopSchema);
+
+app.use(express.json());
+app.use(cors({
+    origin: '*',
+    method: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
+
+app.get('/', async (req, res) => {
+    const page = req.query.p || 1;
+    const q = JSON.parse(req.query.q);
+
+    let options = {};
+
+    options.active = req.query.active;
+    q._id ? options._id = { "$regex": `${q._id}` } : '';
+    q.commerce ? options.commerce = { "$regex": `${q.commerce}` } : '';
+    q.cuit ? options.cuit = { "$regex": `${q.cuit}` } : '';
+
+    const shops = await shopMdel.paginate(options, { page: page, limit: 14})
+    res.status(200).json({
+        shops,
+        total: shops.length
+    })
+})
+
+app.listen(3001, () => console.log('server in port 3001'));
